748df72800d2abba6e5144653993472d328ce814
yashvant <yashvantyadav855@gmail.com>
2023-11-13 08:56:19 +0530

    creating REST controller

---
+++ src/main/java/com/yashvant/emailapi/controller/EmailController.java
@@ -0,0 +1,13 @@
+package com.yashvant.emailapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Email API";
+    }
+}
