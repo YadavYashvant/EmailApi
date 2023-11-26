5a71cc25a8fcfeb9cf531b9d22ce0ae680533bdc
yashvant <yashvantyadav855@gmail.com>
2023-11-26 17:54:48 +0530

    It finally works!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,109 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { NextUIProvider } from '@nextui-org/react'
+import './components/form'
 
 import { Button,Input,Textarea } from '@nextui-org/react'
+import { ToastContainer,toast } from 'react-toastify'
+import AddCourse from './components/form'
 
 function App() {
   const [count, setCount] = useState(0)
   const [theme, setTheme] = useState('light')
 
+  const notify = () => toast.success("🦄 Wow so easy!");
+  useEffect(() => {
+    const theme = localStorage.getItem('theme')
+    if (theme) {
+      setTheme(theme)
+    }
+  });
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const formData = Object.fromEntries(data.entries());
+    console.log(formData);
+  }
   return (
-    <NextUIProvider>
-    <div className="w-full h-[700px] px-8 rounded-3xl flex flex-col justify-center items-center bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
-      <Input
-        className='w-full block '
-        label=""
-        isClearable
-        radius="lg"
-        classNames={{
-          label: "text-black/50 dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/50",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
-        placeholder="Enter recipient's mail"
-        
-      />
-
-      <Input
-        className='w-full block my-5 py-5'
-        label=""
-        isClearable
-        radius="lg"
-        classNames={{
-          label: "text-black/50 dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/50",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
-        placeholder="Enter short and precise subject"
-        
-      />
-
-      <Textarea
-       label="Message"
-        placeholder="Enter your message"
-        className='w-full block my-5 py-5 h-60'
-        radius="lg"
-        classNames={{
-          label: "text-black/50 dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/50",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
-       
-      />
-      
-    </div>
-    </NextUIProvider>
+      <AddCourse/>
   )
 }
 
